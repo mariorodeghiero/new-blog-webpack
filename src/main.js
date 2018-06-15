7e9282683b98d57fbe4e8bdbeb41762ba6b3adf7
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-06-14 22:14:45 -0300

    created files db/ js/ about.html

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
-import notification from "./js/notification";
+// import notification from "./js/notification";
 import "./main.scss";
 
-notification.log("Powered by Mário A. A. Rodeghiero");
-notification.notify("webpack boiler-plate");
+// notification.log("Powered by Mário A. A. Rodeghiero");
+// notification.notify("webpack boiler-plate");
 
-console.log("------------------------------------");
-console.log(4 * 5);
-console.log("------------------------------------");
+
